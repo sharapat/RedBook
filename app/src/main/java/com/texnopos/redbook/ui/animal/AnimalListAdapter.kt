5681dc45ab2091747bd1fc1beaa1269cf7420d71
Sharapat Kalabaev <kalabaevshj@gmail.com>
2020-06-05 06:33:11 +0500

    images are set

--- app/src/main/java/com/texnopos/redbook/ui/animal/AnimalListAdapter.kt
+++ app/src/main/java/com/texnopos/redbook/ui/animal/AnimalListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.texnopos.redbook.R
 import com.texnopos.redbook.data.model.Animal
 import kotlinx.android.synthetic.main.item_animal.view.*
@@ -32,6 +33,12 @@ class AnimalListAdapter : RecyclerView.Adapter<AnimalListAdapter.AnimalListViewH
             itemView.tvUzbName.text = animal.nameUzb
             itemView.tvRusName.text = animal.nameRus
             itemView.tvEngName.text = animal.nameEng
+            val imageResName = "picture${animal.id}"
+            Glide
+                .with(itemView)
+                .load(itemView.context.resources.getIdentifier(imageResName, "drawable", itemView.context.packageName))
+                .into(itemView.ivAnimal)
+
         }
     }
 }
