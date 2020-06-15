5b221a9c0924db833ff48c0a253bbed53be8141e
sharapat <kalabaevshj@gmail.com>
2020-06-15 14:53:23 +0500

    ready

--- app/src/main/java/com/texnopos/redbook/ui/animal/AnimalFragment.kt
+++ app/src/main/java/com/texnopos/redbook/ui/animal/AnimalFragment.kt
@@ -2,9 +2,8 @@ package com.texnopos.redbook.ui.animal
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.texnopos.redbook.R
@@ -37,6 +36,10 @@ class AnimalFragment : Fragment(R.layout.fragment_animal) {
             myAdapter.models = it
         }
         presenter.getAllAnimals(type)
+        etSearch.addTextChangedListener {
+            val result : List<Animal> = dao.searchAnimalByName(type, "${it.toString()}%")
+            myAdapter.models = result
+        }
     }
 
 }
