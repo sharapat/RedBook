90c3daa274edd73ac739c1278b826d880022ce9d
sharapat <kalabaevshj@gmail.com>
2020-06-29 17:44:27 +0500

    lambda

--- app/src/main/java/com/texnopos/redbook/ui/animal/AnimalListAdapter.kt
+++ app/src/main/java/com/texnopos/redbook/ui/animal/AnimalListAdapter.kt
@@ -9,13 +9,21 @@ import com.texnopos.redbook.R
 import com.texnopos.redbook.data.model.Animal
 import kotlinx.android.synthetic.main.item_animal.view.*
 
-class AnimalListAdapter(private val listener: AnimalItemClickListener) : RecyclerView.Adapter<AnimalListAdapter.AnimalListViewHolder>() {
+class AnimalListAdapter : RecyclerView.Adapter<AnimalListAdapter.AnimalListViewHolder>() {
 
     var models: List<Animal> = listOf()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
+
+    private var onItemClick: (animalId: Int) -> Unit = {
+        println("onItemClick ele realizatsiya qilinbadi")
+    }
+
+    fun setOnItemClickListener(a: (animalId: Int) -> Unit) {
+        this.onItemClick = a
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalListViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_animal, parent, false)
@@ -40,7 +48,7 @@ class AnimalListAdapter(private val listener: AnimalItemClickListener) : Recycle
                 .into(itemView.ivAnimal)
 
             itemView.setOnClickListener {
-                listener.onAnimalItemClick(animal.id)
+                onItemClick.invoke(animal.id)
             }
 
         }
--- app/src/main/java/com/texnopos/redbook/ui/animal/AnimalView.kt
+++
@@ -1,7 +0,0 @@
-package com.texnopos.redbook.ui.animal
-
-import com.texnopos.redbook.data.model.Animal
-
-interface AnimalView {
-    fun setData(models: List<Animal>)
-}
